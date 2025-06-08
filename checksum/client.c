ccb840cd0e3a204c54c6c115fd72014c53d36871
lord-cyclone100 <sang66deb@gmail.com>
2025-06-08 10:45:18 +0530

    added checksum client

--- checksum/client.c
+++ checksum/client.c
@@ -2,27 +2,86 @@
 #include<string.h>
 #include<arpa/inet.h>
 #include<unistd.h>
+#include <stdlib.h>
 int main(){
-    int sd,cadl,block;
+    int sd,cadl,datalen,seglen,sum[100],carry,tempsum,k;
     struct sockaddr_in sad,cad;
-    char str[100],check[100]="";
+    char data[100],tempdata[100];
     sd=socket(AF_INET,SOCK_STREAM,IPPROTO_TCP);
     sad.sin_family=AF_INET;
     sad.sin_port=htons(9995);
     sad.sin_addr.s_addr=inet_addr("127.0.0.1");
     connect(sd, (struct sockaddr *)&sad, sizeof(sad));
     printf("Enter binary string : ");
-    scanf("%s",str);
-    printf("Enter the number of blocks ");
-    scanf("%d",&block);
-    int rem = strlen(str)%block;
+    scanf("%s",data);
+    datalen = strlen(data);
+    printf("Enter segment length : ");
+    scanf("%d",&seglen);
+    if(seglen>datalen){
+        printf("Segment length cannot be greater than data length");
+        exit(0);
+    }
+    int rem = datalen%seglen;
     if(rem != 0){
-        for(int i=0;i<(block-rem);i++){
-            strcat(check,"0");
+        int j = seglen-rem;
+        for(int i=0;i<j;i++){
+            tempdata[i] = '0';
+        }
+        tempdata[j] = '\0';
+        strcat(tempdata,data);
+        strcpy(data,tempdata);
+        datalen = strlen(data);
+    }
+
+    // printf("%s",data);
+    // printf("\n%d",datalen);
+
+    for(int i=0;i<seglen;i++){
+        sum[i] = 0;
+    }
+
+
+    for(int i=datalen;i>0;i=i-seglen){
+        k = seglen - 1;
+        carry = 0;
+        for(int j=i-1;j>(i-seglen);j--){
+            tempsum = sum[k] + (data[j]-48) + carry;
+            sum[k] = tempsum%2;
+            carry = tempsum/2;
+            k--;
+        }
+        if(carry == 1){
+            for(int j=(seglen-1);j>=0;j--){
+                tempsum = sum[j] + carry;
+                sum[j] = tempsum%2;
+                carry = tempsum/2;
+            }
         }
     }
-    strcat(check,str);
-    printf("%s",check);
-    send(sd, check, sizeof(check),0);
+
+    for(int i=0;i<seglen;i++){
+        printf("%d",sum[i]);
+    }
+    printf("\n");
+
+    for(int i=0;i<seglen;i++){
+        if(sum[i] == 0){
+            sum[i] = 1;
+        }
+        else{
+            sum[i] = 0;
+        }
+        data[datalen+i] = sum[i]+48;
+    }
+    datalen = strlen(data);
+    data[datalen] = '\0';
+
+
+    printf("%s",data);
+
+
+
+
+    send(sd, data, sizeof(data),0);
     close(sd);
 }
